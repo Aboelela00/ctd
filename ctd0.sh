1bd4ce194064a3738e514b714621905323c4fa97
Aboelela00 <omaraboelela00@gmail.com>
2024-11-25 23:26:31 +0200

    Add files via upload

---
+++ ctd0.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "=== Current Date and Time ==="
+date 
+echo 
+
+echo "=== System Uptime ==="
+uptime
+echo 
+
+echo "=== Logged in Users ==="
+who
+echo 
+
+echo "=== Disk Usage ==="
+
+df/
+
